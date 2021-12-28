f2bad170bf43cbfcf82998276d8981429a44e024
bernardo <bernardoli266@gmail.com>
2021-12-28 19:02:05 +0800

    nacos

--- user-service/src/main/java/cn/itcast/user/mapper/UserMapper.java
+++ user-service/src/main/java/cn/itcast/user/mapper/UserMapper.java
@@ -3,7 +3,9 @@ package cn.itcast.user.mapper;
 import cn.itcast.user.pojo.User;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface UserMapper {
     
     @Select("select * from tb_user where id = #{id}")
